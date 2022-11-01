bfe14ab0740c61327cf5d50d3fefe1afe1554105
Jeff0327 <95613344+Jeff0327@users.noreply.github.com>
2022-11-02 04:54:38 +0900

    update

--- pages/LoginPage.js
+++ pages/LoginPage.js
@@ -4,12 +4,27 @@ export default function LoginPage({navigation}){
 
     return (
         <View>
-            <TouchableOpacity onPress={()=>{navigation.navigate("JoinPage")}}>
-                <Text>회원가입</Text>
+            <View style={styles.loginForm}>
+                <View style={{flexDirection:"row" ,marginBottom:20,}}>
+                    <Text style={styles.text}>아이디:</Text><TextInput style={styles.text} placeholder="아이디를 입력하세요"/>
+                </View>
+                <View style={{flexDirection:"row"}}>
+                    <Text style={styles.text}>비밀번호:</Text><TextInput style={styles.text} placeholder="비밀번호를 입력하세요"/>
+                </View>
+                <View style={{flexDirection:"row"}}>
+                    <Text style={styles.text}>비밀번호확인:</Text><TextInput style={styles.text} placeholder="비밀번호를 입력하세요"/>
+                </View>
+            
+            </View>
+            <View style={styles.loginFormBtn}>
+            <TouchableOpacity style={styles.loginBtn}>
+                <Text style={styles.text}>로그인</Text>
             </TouchableOpacity>
-            <TouchableOpacity>
-                <Text>로그인</Text>
+            <TouchableOpacity style={styles.loginBtn} onPress={()=>{navigation.navigate("JoinPage")}}>
+                <Text style={styles.text}>회원가입</Text>
             </TouchableOpacity>
+            </View>
+            
             <TouchableOpacity>
                 <Text>아이디찾기</Text>
             </TouchableOpacity>
@@ -20,11 +35,32 @@ export default function LoginPage({navigation}){
                 <Text>구글로그인</Text>
             </TouchableOpacity>
             <TouchableOpacity>
-                <Text>폰로그인</Text>
-            </TouchableOpacity>
-            <TouchableOpacity>
                 <Text>Facebook</Text>
             </TouchableOpacity>
         </View>
     )
 }
+const styles = StyleSheet.create({
+    loginForm:{
+        margin:20,
+        alignSelf:"center",
+    },
+    loginFormBtn:{
+        margin:30,
+        flexDirection:"row",
+        justifyContent:"space-around",
+        
+    },
+    text:{
+        fontSize:15,
+        textAlign:"center",
+        
+    },
+    loginBtn:{
+        width:200,
+        height:50,
+        borderRadius:3,
+        backgroundColor:"pink",
+        justifyContent:"center",
+    }
+  });
